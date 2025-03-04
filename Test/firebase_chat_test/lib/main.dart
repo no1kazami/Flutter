4ffa76d922b73ee1bee6a5b63dbca956930e43c8
CKH-DESKTOP-560\using <usinggo@gmail.com>
2025-03-04 11:43:35 +0900

    Initial commit

---
+++ Test/firebase_chat_test/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_chat_test/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginSignupScreen(),
+    );
+  }
+}
